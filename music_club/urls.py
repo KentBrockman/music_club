012d9394f243bbb52d1a209fef640f36268707a6
Kent Brockman <brockman.ks@gmail.com>
2020-10-24 12:47:41 -0600

    minor notes

--- music_club/urls.py
+++ music_club/urls.py
@@ -4,4 +4,12 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    # login page
+    # home page for user
+    # master list for listening group
+    # album page view
+    # album submission page - create Album and AlbumSubmission
+    # users album review view
+    # users album review form
+    # view themes
 ]
--- music_club/views.py
+++ music_club/views.py
@@ -1,7 +1,11 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseServerError
 from django.shortcuts import render
 
 
 # Create your views here.
 def index(request):
+    # print(request.META.REMOTE_USER)
     return HttpResponse("Hello Everyone")
+
+def not_implemented(request):
+    return HttpResponseServerError(f"{request.path} Not Implemented yet", 501)
