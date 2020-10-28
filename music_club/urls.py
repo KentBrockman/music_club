c963b4725410fc76888ab9f6cd0f249eea0aa469
Kent Brockman <brockman.ks@gmail.com>
2020-10-28 13:33:01 -0600

    Experimening with main view and linking

--- music_club/urls.py
+++ music_club/urls.py
@@ -5,11 +5,14 @@ from . import views
 
 urlpatterns = [
     # home page
-    path('', TemplateView.as_view(template_name='home.html'), name='home'),
+    path('', views.MainPage.as_view(), name='home'),
     # master list for listening group
     # album page view
     # album submission page - create Album and AlbumSubmission
-    # users album review view
     # users album review form
-    # view themes
+
+    # themes
+    # view
+    # vote
+    # submit/manage
 ]
--- music_club/views.py
+++ music_club/views.py
@@ -1,5 +1,8 @@
 from django.http import HttpResponse, HttpResponseServerError
 from django.shortcuts import render
+from django.views import generic
+
+from . import models
 
 
 # Create your views here.
@@ -9,3 +12,10 @@ def index(request):
 
 def not_implemented(request):
     return HttpResponseServerError(f"{request.path} Not Implemented yet", 501)
+
+class MainPage(generic.ListView):
+    template_name = "home.html"
+    context_object_name = "albumsubmissions"
+
+    def get_queryset(self):
+        return models.AlbumSubmission.objects.filter().order_by('-round')[:20]
