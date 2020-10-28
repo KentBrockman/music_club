75e0f026da1b8135ab4bed36c5ac32243fe353aa
Kent Brockman <brockman.ks@gmail.com>
2020-10-28 12:30:02 -0600

    project level login/logout implementation

--- music_club/urls.py
+++ music_club/urls.py
@@ -1,11 +1,11 @@
-from django.urls import path
+from django.urls import include, path
+from django.views.generic.base import TemplateView
 
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    # login page
-    # home page for user
+    # home page
+    path('', TemplateView.as_view(template_name='home.html'), name='home'),
     # master list for listening group
     # album page view
     # album submission page - create Album and AlbumSubmission
